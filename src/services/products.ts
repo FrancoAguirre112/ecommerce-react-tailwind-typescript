1220a5ae8795c28106f4bbb7ca794a6b9612d3ec
Franco Aguirre <francoaguirretrabajo@gmail.com>
2023-08-14 19:07:03 -0300

    Update products.ts

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,7 +1,11 @@
 import { ProductType } from "../types";
 export const productsFetch = async (page: number, category: string) => {
   try {
-    const response = await fetch(`${process.env.VITE_PRODUCT_LIST_API}${category && category !== "all" ? `/category/${category}` : ""}?limit=${6*page}`);
+    const response = await fetch(
+      `${import.meta.env.VITE_PRODUCT_LIST_API}${
+        category && category !== "all" ? `/category/${category}` : ""
+      }?limit=${6 * page}`
+    );
     const json = await response.json();
 
     const products = json;
@@ -12,7 +16,11 @@ export const productsFetch = async (page: number, category: string) => {
       description: product.description,
       price: product.price.toFixed(2),
       category: product.category,
-      image: ["https://i.imgur.com/iePzniM.png", "https://i.imgur.com/xH3dQso.png", "https://i.imgur.com/YCg9tL6.png"],
+      image: [
+        "https://i.imgur.com/iePzniM.png",
+        "https://i.imgur.com/xH3dQso.png",
+        "https://i.imgur.com/YCg9tL6.png",
+      ],
     }));
   } catch (error) {
     throw new Error("Error fetching products");
