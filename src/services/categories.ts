e020c115294bc901cdae72ec5d1060525de74bde
Franco Aguirre <francoaguirretrabajo@gmail.com>
2023-08-14 19:12:05 -0300

    Update categories.ts

--- src/services/categories.ts
+++ src/services/categories.ts
@@ -1,6 +1,6 @@
 export const categoriesFetch = async() => {
     try{
-        const response = await fetch("https://fakestoreapi.com/products/categories");
+        const response = await fetch(import.meta.env.VITE_PRODUCT_LIST_API);
         const json = await response.json();
 
         const categories = json;
